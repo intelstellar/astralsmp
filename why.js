b69205ec4bdcb42700ab5d835055414cbe191778
illithixer <85721047+intelstellar@users.noreply.github.com>
2023-08-03 16:41:04 +0700

    Create why.js

---
+++ why.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Project is running!");
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+})
+
+const Discord = require("discord.js");
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+
+client.on("message", message => {
+  if(message.content === "~axol") {
+    message.channel.send("Why kronieo#0001 is the best ww? ")
+  }
+})
+
+client.login(process.env.token)
